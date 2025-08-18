fbb952793289c55843a0d5e7cd22ac3200fc5eff
Lana Avery <memekoten@gmail.com>
2025-08-19 00:08:38 +1000

    chore: cleaned up old properties

--- Source/InterchangeableItems.cs
+++ Source/InterchangeableItems.cs
@@ -14,14 +14,8 @@ namespace Replace_Stuff
 
 	public class ReplaceList
 	{
-		public bool isWall = false;
-
-		public bool isWorkbench = false;
-
 		public string category = "";
-
 		public List<ThingDef> items = new();
-
 		public List<string> comps = new();
 	}
 }
